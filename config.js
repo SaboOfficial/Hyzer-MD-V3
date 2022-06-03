42b3aeff637e74db3cfa7136010fbab663312f8e
Sabo Ofc <103872974+SaboBOTZMD@users.noreply.github.com>
2022-06-03 20:46:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,32 +1,32 @@
 //=============『 Utama 』================== //
-global.owner = ['6287892711054'] 
+global.owner = ['628815952469'] 
 global.mods = [] 
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Hyzer Official'
-global.numberowner = '6287892711054'
-global.instagram = 'https://instagram.com/hyzer_stfu'
-global.github = 'https://github.com/Hyzerr'
-global.dana = '087892711054'
-global.pulsa = '085212023766'
-global.gopay = '087892711054'
+global.nameowner = 'Sabo'
+global.numberowner = '628815952469'
+global.instagram = 'https://instagram.com/dzycassano'
+global.github = '-'
+global.dana = '0821-4520-3493'
+global.pulsa = '0821-4520-3493'
+global.gopay = '0821-4520-3493'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ShiroBot-Md'
-global.gc = 'https://chat.whatsapp.com/Jzd9DEVB5nODtNBk1VCNrV'
-global.web = 'https://hyzerr-apii.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
+global.namebot = 'Sabo - MD'
+global.gc = 'https://chat.whatsapp.com/BXRH5ApRnbaHm6ULbPuG4d'
+global.web = 'https://chat.whatsapp.com/BXRH5ApRnbaHm6ULbPuG4d' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.price1 = '1 Minggu = 5.000\n1 Bulan = 8.000\nPermanent = 12.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
 global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm = 'Sabo - MD'
 global.watermark = wm
-global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '⫹⫺ 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm2 = '                     「 Sabo - MDあ⁩ 」'
+global.wm3 = '⫹⫺ Sabo - MD'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
@@ -36,8 +36,8 @@ global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = '2022'
-global.author = '© Hyzer Official'
+global.packname = 'Sabo'
+global.author = 'Sabo - MD'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
