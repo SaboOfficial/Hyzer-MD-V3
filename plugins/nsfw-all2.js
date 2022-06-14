18f2e080eaf4b6918b8c9c2dd8c753661b7782b5
Sabo Ofc <103872974+SaboBOTZMD@users.noreply.github.com>
2022-06-14 18:34:46 +0800

    Add files via upload

--- plugins/nsfw-all2.js
+++ plugins/nsfw-all2.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://api.lolhuman.xyz/api/random/nsfw/${command}?apikey=${lolkey}`
-    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
+    conn.sendButtonImg(m.chat, nyenye, 'Si Kontol Sange Ama Kartun Ngentot', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['hollolewd', 'sideoppai', 'animefeets', 'animebooty', 'animethighss', 'animearmpits', 'lewdanimegirls', 'biganimetiddies']
 handler.tags = ['nsfw']
--- plugins/nsfw-all3.js
+++ plugins/nsfw-all3.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://api.lolhuman.xyz/api/random2/${command}?apikey=${lolkey}`
-    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
+    conn.sendButtonImg(m.chat, nyenye, 'Si Kontol Sange Ama Kartun Ngentot', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['lewd', 'eron', 'solo', 'anal', 'keta', 'tits', 'kuni', 'solog', 'erok', 'feetg', 'lewdk', 'erofeet', 'holoero', 'classic', 'erokemo', 'futanari', 'eroyuri', 'yaoi']
 handler.tags = ['nsfw']
