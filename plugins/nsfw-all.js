af25ae251f1bf991bd2508ae39f6249daea3476f
Sabo Ofc <107702229+SaboOfficial@users.noreply.github.com>
2022-06-21 19:38:37 +0800

    Update nsfw-all.js

--- plugins/nsfw-all.js
+++ plugins/nsfw-all.js
@@ -5,5 +5,5 @@ let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
 handler.tags = ['nsfw']
 handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|hentai|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri)$/i
-handler.premium = true
+handler.limit = true
 module.exports = handler
