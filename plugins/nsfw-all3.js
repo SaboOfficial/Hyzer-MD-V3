28742898cb87386b7c584cc163b6eb0bb02e0822
Sabo Ofc <107702229+SaboOfficial@users.noreply.github.com>
2022-06-21 19:39:03 +0800

    Update nsfw-all3.js

--- plugins/nsfw-all3.js
+++ plugins/nsfw-all3.js
@@ -5,6 +5,6 @@ let nyenye = `https://api.lolhuman.xyz/api/random2/${command}?apikey=${lolkey}`
 handler.help = ['lewd', 'eron', 'solo', 'anal', 'keta', 'tits', 'kuni', 'solog', 'erok', 'feetg', 'lewdk', 'erofeet', 'holoero', 'classic', 'erokemo', 'futanari', 'eroyuri', 'yaoi']
 handler.tags = ['nsfw']
 handler.command = /^(lewd|eron|solo|anal|keta|tits|kuni|solog|erok|feetg|lewdk|erofeet|holoero|classic|erokemo|futanari|eroyuri|yaoi)$/i
-handler.premium = true
+handler.limit = true
 module.exports = handler
  
