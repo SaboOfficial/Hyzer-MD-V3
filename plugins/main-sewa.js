87f15e7b0ab49bfddceab6754ff1f5b47a973c18
Sabo Ofc <107702229+SaboOfficial@users.noreply.github.com>
2022-06-23 03:13:52 +0800

    Add files via upload

---
+++ plugins/main-sewa.js
@@ -0,0 +1,35 @@
+let handler = async (m, { conn }) => {
+  let fetch = require('node-fetch')
+  let a = await conn.profilePictureUrl(conn.user.jid, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let b = await conn.profilePictureUrl(owner[0]+'@s.whatsapp.net', 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
+  let c = pickRandom([a, b])
+  let d = await fetch(c).then(a => a.buffer())
+  let prepare = await require('@adiwajshing/baileys').generateWAMessageFromContent(m.key.remoteJid,{listMessage:{
+  title: `${await conn.getName(conn.user.jid)}`,
+  description: ` *• SEWA BOT •*
+        
+Jan Lupa Join Group Bot Kami
+`,
+  buttonText: 'Harga Dh Murah Ykli Nego',
+  listType: 2,
+  productListInfo: {
+  productSections: [{
+  title:'Klik untuk order',
+  products:[{productId:'5370437279687043'}]}],
+  headerImage: { productId: '4900004496777133', 
+  jpegThumbnail: d },
+  businessOwnerJid: `${owner[0]}@s.whatsapp.net`
+  },
+  footerText: 'https://github.com/SaboOfficial',
+  }},{})
+  conn.relayMessage(prepare.key.remoteJid,prepare.message,{messageId:prepare.key.id})
+}
+handler.help = ['sewa' , 'sewabot']
+handler.tags = ['main']
+handler.command = /^(sewa|rent(this)?(bot)?)$/i
+
+module.exports = handler
+
+function pickRandom(list) {
+        return list[Math.floor(Math.random() * list.length)]
+    }
